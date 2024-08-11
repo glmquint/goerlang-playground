b999052d6bbc0562eb50e19193b58168beff9012
Guillaume Quint <g.quint@studenti.unipi.it>
2024-08-12 00:03:56 +0200

    direct send from go to erlang mailbox

--- main.go
+++ main.go
@@ -22,7 +22,12 @@ func init() {
 }
 
 func main() {
-	flag.Parse()
+	// go_node_id := "go_c0ecdfb7-00f1-4270-8e46-d835bd00f153@127.0.0.1"       // os.Args[0]
+	// erl_client_name := "director@127.0.0.1"                                 // os.Args[1]
+	// erl_worker_mailbox := "mboxserver_c0ecdfb7-00f1-4270-8e46-d835bd00f153" // os.Args[2]
+	// erl_cookie := "cookie_123456789"                                        // os.Args[3]
+	// experiment_id := "c0ecdfb7-00f1-4270-8e46-d835bd00f153"                 // os.Args[4]
+	// flag.Parse()
 
 	fmt.Println("")
 	fmt.Println("to stop press Ctrl-C")
@@ -33,13 +38,23 @@ func main() {
 		panic(err)
 	}
 
-	_, err = node.Spawn(ServerName, gen.ProcessOptions{}, &demo{})
+	proc, err := node.Spawn(ServerName, gen.ProcessOptions{}, &gen.Server{})
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Start erlang node with the command below:")
 	fmt.Printf("    $ erl -name %s -setcookie %s\n\n", "erl-"+node.Name(), Cookie)
+
+	mailbox := "mailbox"
+	dest_node := "server@127.0.0.1"
+	err = proc.Send(gen.ProcessID{Name: mailbox, Node: dest_node}, "hello from go")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Sent a message to {%s, '%s'}, use flush(). to read it!\n", mailbox, dest_node)
+	fmt.Printf("(Mailbox must be already registered using register(%s, self()).\n\n", mailbox)
 
 	fmt.Println("----- to make call request from 'erl'-shell:")
 	fmt.Printf("gen_server:call({%s,'%s'}, hi).\n", ServerName, NodeName)
